7e395e4abd1e701f2f2c9191ecaed30b57a87a63
E1P <ewan.pearce@gmail.com>
2019-04-01 16:40:03 +0100

    Create index file to export all components

---
+++ src/Components/index.js
@@ -0,0 +1,8 @@
+import Articles from "./Articles/Articles";
+import CurrentDate from "./CurrentDate/CurrentDate";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import NavBar from "./NavBar/NavBar";
+import TopArticles from "./TopArticles/TopArticles";
+
+export { Articles, CurrentDate, Footer, Header, NavBar, TopArticles };
