9cc5c0bb8ba5996d4a813d132770e3d59e3bbaae
E1P <ewan.pearce@gmail.com>
2019-04-01 22:59:28 +0100

    Introduce dummy article data

---
+++ src/data-api/dummy-data.js
@@ -0,0 +1,137 @@
+export const articles = [
+  {
+    article_id: 52,
+    author: "grumpy19",
+    created_at: "2019-03-22T00:00:00.000Z",
+    title: "It's a game of two halves",
+    topic: "football",
+    votes: 6,
+    comment_count: "3"
+  },
+  {
+    article_id: 51,
+    author: "grumpy19",
+    created_at: "2019-03-22T00:00:00.000Z",
+    title: "Goalposts",
+    topic: "football",
+    votes: 8,
+    comment_count: "1"
+  },
+  {
+    article_id: 33,
+    author: "weegembump",
+    created_at: "2018-05-30T00:00:00.000Z",
+    title: "Seafood substitutions are increasing",
+    topic: "cooking",
+    votes: 0,
+    comment_count: "6"
+  },
+  {
+    article_id: 28,
+    author: "happyamy2016",
+    created_at: "2018-05-27T00:00:00.000Z",
+    title: "High Altitude Cooking",
+    topic: "cooking",
+    votes: 0,
+    comment_count: "4"
+  },
+  {
+    article_id: 30,
+    author: "jessjelly",
+    created_at: "2018-05-06T00:00:00.000Z",
+    title: "Twice-Baked Butternut Squash Is the Thanksgiving Side Dish of Your Dreams",
+    topic: "cooking",
+    votes: 0,
+    comment_count: "8"
+  },
+  {
+    article_id: 13,
+    author: "weegembump",
+    created_at: "2018-04-16T00:00:00.000Z",
+    title: "What does Jose Mourinho's handwriting say about his personality?",
+    topic: "football",
+    votes: 0,
+    comment_count: "6"
+  },
+  {
+    article_id: 18,
+    author: "grumpy19",
+    created_at: "2018-03-28T00:00:00.000Z",
+    title: "The People Tracking Every Touch, Pass And Tackle in the World Cup",
+    topic: "football",
+    votes: 1,
+    comment_count: "8"
+  },
+  {
+    article_id: 6,
+    author: "grumpy19",
+    created_at: "2018-03-14T00:00:00.000Z",
+    title: "JavaScript’s Apply, Call, and Bind Methods are Essential for JavaScript Professionals",
+    topic: "coding",
+    votes: 0,
+    comment_count: "11"
+  },
+  {
+    article_id: 16,
+    author: "weegembump",
+    created_at: "2018-02-17T00:00:00.000Z",
+    title: "History of FC Barcelona",
+    topic: "football",
+    votes: 0,
+    comment_count: "16"
+  },
+  {
+    article_id: 4,
+    author: "jessjelly",
+    created_at: "2017-12-24T00:00:00.000Z",
+    title: "Making sense of Redux",
+    topic: "coding",
+    votes: 0,
+    comment_count: "9"
+  },
+  {
+    article_id: 11,
+    author: "tickle122",
+    created_at: "2017-11-10T00:00:00.000Z",
+    title: "Designing Better JavaScript APIs",
+    topic: "coding",
+    votes: 0,
+    comment_count: "5"
+  },
+  {
+    article_id: 31,
+    author: "tickle122",
+    created_at: "2017-11-05T00:00:00.000Z",
+    title: "What to Cook This Week",
+    topic: "cooking",
+    votes: 0,
+    comment_count: "12"
+  },
+  {
+    article_id: 20,
+    author: "tickle122",
+    created_at: "2017-10-10T00:00:00.000Z",
+    title: "History of Football",
+    topic: "football",
+    votes: 0,
+    comment_count: "5"
+  },
+  {
+    article_id: 34,
+    author: "grumpy19",
+    created_at: "2017-08-16T00:00:00.000Z",
+    title: "The Notorious MSG’s Unlikely Formula For Success",
+    topic: "cooking",
+    votes: 0,
+    comment_count: "11"
+  },
+  {
+    article_id: 32,
+    author: "grumpy19",
+    created_at: "2017-08-16T00:00:00.000Z",
+    title: "Halal food: Keeping pure and true",
+    topic: "cooking",
+    votes: 0,
+    comment_count: "5"
+  }
+];
