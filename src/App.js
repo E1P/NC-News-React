1bd9e1401c2990174af888c91bb9042d4499b1fb
E1P <ewan.pearce@gmail.com>
2019-04-01 15:58:47 +0100

    Set up Component files for main app view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
