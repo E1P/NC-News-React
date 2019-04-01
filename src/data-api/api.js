6e5566d0988157b5da2ed77e6ea44f26602949df
E1P <ewan.pearce@gmail.com>
2019-04-01 22:59:58 +0100

    Copy over api file with axios setup

---
+++ src/data-api/api.js
@@ -0,0 +1,86 @@
+import axios from "axios";
+
+const request = axios.create({
+  baseURL: "https://e1p-nc-news.herokuapp.com/api"
+});
+
+export const getArticles = params => {
+  console.log("params>>>", params);
+  return request
+    .get("/articles", { params })
+    .then(({ data }) => {
+      return data;
+    })
+    .catch(err => console.log("Error from axios >>> ", err));
+};
+
+export const getArticleById = id => {
+  return request
+    .get(`/articles/${id}`)
+    .then(({ data }) => {
+      return data;
+    })
+    .catch(err => console.log("Error from axios >>> ", err));
+};
+
+export const getCommentsByArticleId = id => {
+  return request
+    .get(`/articles/${id}/comments`)
+    .then(({ data }) => {
+      return data;
+    })
+    .catch(err => console.log("Error from axios >>> ", err));
+};
+
+export const postNewArticle = article => {
+  // const test = {
+  //   title: "It's a game of two halves",
+  //   topic: "football",
+  //   author: "grumpy19",
+  //   body: "One half lasts approximately 45 minutes. The second half requires similar levels of endurance."
+  // };
+  return request
+    .post("/articles", article)
+    .then(({ data }) => data)
+    .catch(err => console.log("Error from axios >>> ", err.response));
+};
+
+export const deleteArticle = id => {
+  return request
+    .delete(`/articles/${id}`)
+    .then(res => res)
+    .catch(err => console.log("Error from axios >>> ", err.response));
+};
+
+export const postComment = (id, comment) => {
+  return request
+    .post(`/articles/${id}/comments`, comment)
+    .then(res => res)
+    .catch(err => console.log("Error from axios >>> ", err.response));
+};
+
+export const deleteComment = id => {
+  return request
+    .delete(`/comments/${id}`)
+    .then(res => res)
+    .catch(err => console.log("Error from axios >>> ", err.response));
+};
+
+export const getUsers = () => {
+  return request
+    .get(`/users`)
+    .then(({ data }) => {
+      return data;
+    })
+    .catch(err => console.log("Error from axios >>> ", err));
+};
+
+export const incrementVote = (id, vote) => {
+  console.log("vote object: ", vote, id);
+  return request
+    .patch(`/articles/${id}`, vote)
+    .then(({ data }) => {
+      return data;
+    })
+    .catch(err => console.log("Error from axios >>> ", err));
+};
