12427885fa059743196f8d1f261f0c604998fee0
E1P <ewan.pearce@gmail.com>
2019-04-01 16:40:44 +0100

    Set up basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Articles, CurrentDate, Footer, Header, NavBar, TopArticles } from "./Components/index";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Header />
+        <CurrentDate />
+        <NavBar />
+        <TopArticles />
+        <Articles />
+        <Footer />
+      </div>
+    );
   }
 }
 
