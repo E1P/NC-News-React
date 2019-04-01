5df02c0f3cfd44d3322191025f8c0c1552318af3
E1P <ewan.pearce@gmail.com>
2019-04-01 23:52:35 +0100

    Clean up dummy topics

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class NavBar extends Component {
   render() {
-    return <div className="nav">Coding - Cooking - Football +++ Sewing Gardening Climbing </div>;
+    return <div className="nav">All | Coding | Cooking | Football | Sewing | Gardening Climbing </div>;
   }
 }
