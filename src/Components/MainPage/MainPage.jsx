9c8fbb9296ec3484867c7debf28e6f84cc10cecd
E1P <ewan.pearce@gmail.com>
2019-04-02 16:33:19 +0100

    Remove className (sent down through Router)

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import { MainArticles, TopArticles } from "../index";
 
 export default function MainPage() {
   return (
-    <div className="main">
+    <div /* className="router-main" */>
       <TopArticles />
       <MainArticles />
     </div>
