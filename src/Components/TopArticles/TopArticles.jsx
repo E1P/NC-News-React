6aa1af35369f5f400db232778d054a107f76c48c
E1P <ewan.pearce@gmail.com>
2019-04-02 12:34:51 +0100

    Set up class functions and fetch method. Render mapped TopArticlePreviews

--- src/Components/TopArticles/TopArticles.jsx
+++ src/Components/TopArticles/TopArticles.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
+import { getArticles } from "../../data-api/api";
+import { TopArticlePreview } from "../index";
 
 export default class TopArticles extends Component {
+  state = {
+    topArticles: []
+  };
+
+  fetchTopArticles = () => {
+    const params = {
+      sort_by: "comment_count",
+      limit: 3
+    };
+    getArticles(params).then(({ articles }) => {
+      this.setState({ topArticles: articles });
+    });
+  };
+
+  componentDidMount() {
+    this.fetchTopArticles();
+  }
+
   render() {
     return (
       <div className="top-articles">
-        Top Article <br />
-        Top Article <br />
-        Top Article <br />
+        {this.state.topArticles.map(article => {
+          return <TopArticlePreview key={article.article_id} article={article} />;
+        })}
       </div>
     );
   }
