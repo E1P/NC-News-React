fb32e9224b82a3faaaec7d034914b87540a81cf6
E1P <ewan.pearce@gmail.com>
2019-04-02 20:54:35 +0100

    Add topic & limit properties with conditionals

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { MainArticles, TopArticles } from "../index";
 
-export default function MainPage() {
+export default function MainPage(props) {
+  console.log(props);
+  const topic = props.topic ? props.topic : null;
+  const limit = topic ? 1 : 3;
   return (
-    <div /* className="router-main" */>
-      <TopArticles />
-      <MainArticles />
+    <div>
+      <TopArticles topic={topic} limit={limit} />
+      <MainArticles topic={topic} />
     </div>
   );
 }
