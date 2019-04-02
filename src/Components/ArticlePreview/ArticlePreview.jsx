b4384aa97ae8b743b4676c5c3e4a6b58915020b1
E1P <ewan.pearce@gmail.com>
2019-04-02 22:33:26 +0100

    Refactor divs to headers

--- src/Components/ArticlePreview/ArticlePreview.jsx
+++ src/Components/ArticlePreview/ArticlePreview.jsx
@@ -4,12 +4,11 @@ export default function ArticlePreview({ article }) {
   return (
     <section className="article-preview">
       <div>
-        <div>Votes:{article.votes}</div>
-        <div>Comments: {article.comment_count}</div>
-        <div>{article.created_at.slice(0, 10)}</div>
+        <h3>{article.title}</h3>
+        <h5>Votes:{article.votes}</h5>
+        <h5>Comments: {article.comment_count}</h5>
+        <h5>{article.created_at.slice(0, 10)}</h5>
       </div>
-      <div />
-      <div>{article.title}</div>
     </section>
   );
 }
