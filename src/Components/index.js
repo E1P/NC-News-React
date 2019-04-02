3a56eee71093b2c291556bd1ac38036e2b5c49be
E1P <ewan.pearce@gmail.com>
2019-04-02 11:56:22 +0100

    Import/export Loader and Button

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,5 +5,7 @@ import Header from "./Header/Header";
 import NavBar from "./NavBar/NavBar";
 import TopArticles from "./TopArticles/TopArticles";
 import SignIn from "./SignIn/SignIn";
+import Loader from "./Loader/Loader";
+import Button from "./Button/Button";
 
-export { MainArticles, CurrentDate, Footer, Header, NavBar, SignIn, TopArticles };
+export { MainArticles, CurrentDate, Footer, Header, NavBar, SignIn, TopArticles, Loader, Button };
