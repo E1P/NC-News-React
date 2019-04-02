2a53c7006e2489a150295e29ab62a549335bab03
E1P <ewan.pearce@gmail.com>
2019-04-02 14:42:46 +0100

    Refactor router with applicable imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Router } from "@reach/router";
 import "./App.css";
-import { Footer, Header, NavBar, SignIn, MainPage, TopicPage, SignInPage, SingleArticle, ArticleForm } from "./Components/index";
+import { Footer, Header, NavBar, SignIn, MainPage, TopicPage, SignInPage, SingleArticle, Form } from "./Components/index";
 
 class App extends Component {
   render() {
@@ -10,12 +10,12 @@ class App extends Component {
         <Header />
         <SignIn />
         <NavBar />
-        <Router>
-          <TopicPage />
-          <MainPage />
-          <SingleArticle />
-          <ArticleForm />
-          <SignInPage />
+        <Router className="router-main">
+          <MainPage path="/" />
+          <TopicPage path="/topics/:topic" />
+          <SingleArticle path="/articles/:article_id" />
+          <Form path="/form/:type" />
+          <SignInPage path="/sign-in" />
         </Router>
         <Footer />
       </div>
