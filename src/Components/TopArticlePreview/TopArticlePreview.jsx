113d2205b843914ef6fa526539108697012dcabd
E1P <ewan.pearce@gmail.com>
2019-04-02 12:35:36 +0100

    Set up basic render with text

---
+++ src/Components/TopArticlePreview/TopArticlePreview.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+export default class TopArticlePreview extends Component {
+  render() {
+    const { article } = this.props;
+    return (
+      <Link to={`/articles/${article.article_id}`}>
+        <div className="top-article">Top Article</div>
+      </Link>
+    );
+  }
+}
