ecbe568892137bd6ead2fdd2773e586546e5002b
E1P <ewan.pearce@gmail.com>
2019-04-02 22:35:52 +0100

    Clean up bad logic and remove console.log

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { MainArticles, TopArticles } from "../index";
 
 export default function MainPage(props) {
-  console.log(props);
-  const topic = props.topic ? props.topic : null;
+  const topic = props.topic;
   const limit = topic ? 1 : 3;
   return (
     <div>
