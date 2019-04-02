9366b51e5c48a86d83855d80c005d97c481164e8
E1P <ewan.pearce@gmail.com>
2019-04-02 22:33:50 +0100

    Add Link round main text

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "@reach/router";
 import CurrentDate from "../CurrentDate/CurrentDate";
 
 export default function Header() {
   return (
     <div className="header">
-      <h1>NC NEWS</h1>
+      <Link to="/">
+        <h1>NC NEWS</h1>
+      </Link>
       <CurrentDate />
     </div>
   );
