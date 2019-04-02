bf8de5bfd6ab05f2da11133af81fc915b8e4a414
E1P <ewan.pearce@gmail.com>
2019-04-02 22:36:43 +0100

    Refactor to class and set up basic content display

--- src/Components/SingleArticle/SingleArticle.jsx
+++ src/Components/SingleArticle/SingleArticle.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import { getArticleById } from "../../data-api/api";
+import Comments from "../Comments/Comments";
 
-export default function SingleArticle() {
-  return <div>This is an article.....</div>;
+export default class SingleArticle extends Component {
+  state = {
+    article: {}
+  };
+
+  componentDidMount() {
+    this.fetchArticleById();
+  }
+
+  fetchArticleById = () => {
+    const { article_id } = this.props;
+    getArticleById(article_id).then(({ article }) => {
+      this.setState({ article });
+    });
+  };
+
+  render() {
+    const { article } = this.state;
+    const { article_id } = this.props;
+    return (
+      <div>
+        <section className="article">
+          <h6>{article.topic}</h6>
+          <h5>{article.title}</h5>
+          <p>{article.body}</p>
+          <p>Author: {article.author}</p>
+          <p>{article.created_at}</p>
+          <p>Comments so far: {article.comment_count}</p>
+          <p>{article.votes} people like this article.</p>
+        </section>
+        <Comments article_id={article_id} />
+      </div>
+    );
+  }
 }
