3f59d07a4074c788a5afac4db5141f821a674471
E1P <ewan.pearce@gmail.com>
2019-04-03 09:55:26 +0100

    Change height to px rather than percentage

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -3,22 +3,20 @@ import "./loader.css";
 
 export default function Loader() {
   return (
-    <div>
-      <div className="main-container">
-        <div className="rectangle delay7" />
-        <div className="rectangle delay6" />
-        <div className="rectangle delay5" />
-        <div className="rectangle delay4" />
-        <div className="rectangle delay3" />
-        <div className="rectangle delay2" />
-        <div className="rectangle delay1" />
-        <div className="rectangle delay2" />
-        <div className="rectangle delay3" />
-        <div className="rectangle delay4" />
-        <div className="rectangle delay5" />
-        <div className="rectangle delay6" />
-        <div className="rectangle delay7" />
-      </div>
+    <div className="main-container">
+      <div className="rectangle delay7" />
+      <div className="rectangle delay6" />
+      <div className="rectangle delay5" />
+      <div className="rectangle delay4" />
+      <div className="rectangle delay3" />
+      <div className="rectangle delay2" />
+      <div className="rectangle delay1" />
+      <div className="rectangle delay2" />
+      <div className="rectangle delay3" />
+      <div className="rectangle delay4" />
+      <div className="rectangle delay5" />
+      <div className="rectangle delay6" />
+      <div className="rectangle delay7" />
     </div>
   );
 }
