a9f434f5d0b822050faef2e5daec47131a471a4b
E1P <ewan.pearce@gmail.com>
2019-04-03 09:56:15 +0100

    Add ToolBar and import

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { MainArticles, TopArticles } from "../index";
+import { MainArticles, TopArticles, ToolBar } from "../index";
 
 export default function MainPage(props) {
   const topic = props.topic;
   const limit = topic ? 1 : 3;
   return (
     <div>
+      <ToolBar />
       <TopArticles topic={topic} limit={limit} />
       <MainArticles topic={topic} />
     </div>
