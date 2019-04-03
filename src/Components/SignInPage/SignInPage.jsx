192ae73540e552ef6038f503b0a60af0de278a58
E1P <ewan.pearce@gmail.com>
2019-04-03 15:39:27 +0100

    Refactor to class. Set up with methods and form

--- src/Components/SignInPage/SignInPage.jsx
+++ src/Components/SignInPage/SignInPage.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { getUsers } from "../../data-api/api";
 
-export default function SignInPage() {
-  return <div>Sign in here!</div>;
+export default class SignInPage extends Component {
+  state = {
+    users: [],
+    username: "",
+    password: ""
+  };
+
+  componentDidMount() {}
+
+  componentDidUpdate() {}
+
+  handleChange = e => {
+    const { id, value } = e.target;
+    this.setState({ [id]: value });
+  };
+
+  render() {
+    return (
+      <form className="form">
+        <label htmlFor="username">User: </label>
+        <input required id="username" onChange={this.handleChange} />
+        <label htmlFor="password">Password</label>
+        <input required type="password" id="password" onChange={this.handleChange} />
+        <button onSubmit={this.handleSubmit}>Sign in</button>
+      </form>
+    );
+  }
 }
