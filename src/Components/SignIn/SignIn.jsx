8a28685337b3afb0a4fd38d5c723bda41ba15065
E1P <ewan.pearce@gmail.com>
2019-04-04 15:38:37 +0100

    Set up authorisation logic

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Link } from "@reach/router";
 
-export default function SignIn() {
-  return (
+export default function SignIn(props) {
+  const { user, handleAuth } = props;
+  return user ? (
+    <div>
+      <p>Signed in as: {user.username}</p>
+      <Link className="sign-in" to="/sign-in">
+        Profile
+      </Link>
+      <button onClick={() => handleAuth("")}>Sign out</button>
+    </div>
+  ) : (
     <Link className="sign-in" to="/sign-in">
       Sign In
     </Link>
