45a34079a403da09e7972ac4736f58a7c7dfb9c4
E1P <ewan.pearce@gmail.com>
2019-04-04 11:49:37 +0100

    Import Sorter and add to render

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { MainArticles, TopArticles, FormButton } from "../index";
+import { MainArticles, TopArticles, FormButton, Sorter } from "../index";
 
 export default function MainPage(props) {
   const { topic } = props;
   const limit = topic ? 1 : 3;
   return (
     <div>
-      <FormButton type="article" />
+      <div className="toolbar">
+        <Sorter />
+        <FormButton type="article" />
+      </div>
       <TopArticles topic={topic} limit={limit} />
       <MainArticles topic={topic} />
     </div>
