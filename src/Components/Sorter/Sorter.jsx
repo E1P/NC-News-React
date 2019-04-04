0cb346c0d0a855980cbbc0b2c0bc64fb91cdd4e2
E1P <ewan.pearce@gmail.com>
2019-04-04 12:29:19 +0100

    Add buttons for sorting using handleSorting on props

--- src/Components/Sorter/Sorter.jsx
+++ src/Components/Sorter/Sorter.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Sorter() {
-  return <div>Sort-by Asc/Desc</div>;
+export default function Sorter(props) {
+  const { handleSorting } = props;
+  return (
+    <div>
+      <button onClick={() => handleSorting("votes")}>Votes</button>
+      <button onClick={() => handleSorting("comment_count")}>Comments</button>
+      <button onClick={() => handleSorting("created_at")}>Date</button>
+    </div>
+  );
 }
