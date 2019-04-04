6ee4052a7d2ffdc3151a89bd31145b0eb9fad500
E1P <ewan.pearce@gmail.com>
2019-04-04 16:42:29 +0100

    Add classes and refactor arguments given to handleSorting to allow easy state change in parent

--- src/Components/Sorter/Sorter.jsx
+++ src/Components/Sorter/Sorter.jsx
@@ -3,10 +3,22 @@ import React from "react";
 export default function Sorter(props) {
   const { handleSorting } = props;
   return (
-    <div>
-      <button onClick={() => handleSorting("votes")}>Votes</button>
-      <button onClick={() => handleSorting("comment_count")}>Comments</button>
-      <button onClick={() => handleSorting("created_at")}>Date</button>
+    <div className="sorter">
+      <button className="button" onClick={() => handleSorting({ sort_by: "votes" })}>
+        Votes
+      </button>
+      <button className="button" onClick={() => handleSorting({ sort_by: "comment_count" })}>
+        Comments
+      </button>
+      <button className="button" onClick={() => handleSorting({ sort_by: "created_at" })}>
+        Date
+      </button>
+      <button className="button" onClick={() => handleSorting({ order: "asc" })}>
+        Asc
+      </button>
+      <button className="button" onClick={() => handleSorting({ order: "desc" })}>
+        Desc
+      </button>
     </div>
   );
 }
