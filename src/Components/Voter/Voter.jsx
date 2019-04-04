0dc0febee34b6dfe7ed47ce5d973ba2f567e31f6
E1P <ewan.pearce@gmail.com>
2019-04-05 00:56:34 +0100

    Add render conditionals to vote buttons. Import setState logic from SingleArticle

--- src/Components/Voter/Voter.jsx
+++ src/Components/Voter/Voter.jsx
@@ -5,21 +5,29 @@ export default class Voter extends React.Component {
     voteChange: 0
   };
 
+  handleClick = vote => {
+    const { handleVote } = this.props;
+    const voteChange = this.state.voteChange + vote;
+    this.setState({ voteChange }, () => handleVote(vote));
+  };
+
   render() {
-    const { handleVote, votes } = this.props;
+    const { votes } = this.props;
     const { voteChange } = this.state;
     return (
       <div className="voter">
-        <button className="button" onClick={() => handleVote(+1)}>
-          Up
-        </button>{" "}
+        {voteChange !== 1 && (
+          <button className="button" onClick={() => this.handleClick(+1)}>
+            Up
+          </button>
+        )}{" "}
         {votes + voteChange}{" "}
-        <button className="button" onClick={() => handleVote(-1)}>
-          Down
-        </button>
+        {voteChange !== -1 && (
+          <button className="button" onClick={() => this.handleClick(-1)}>
+            Down
+          </button>
+        )}
       </div>
     );
   }
 }
-
-// Change to class with modified vote in state
