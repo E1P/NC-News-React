795439b7bdfc0a30b6a57a55dd9a6932ea0f7509
E1P <ewan.pearce@gmail.com>
2019-04-04 15:40:12 +0100

    Add alternate views & conditional for signed-in/out and wrong inputs

--- src/Components/SignInPage/SignInPage.jsx
+++ src/Components/SignInPage/SignInPage.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { getSingleUser } from "../../data-api/api";
 import { navigate } from "@reach/router";
 
 export default class SignInPage extends Component {
   state = {
     username: "",
-    password: ""
+    password: "",
+    badInput: false
   };
 
   componentDidMount() {}
@@ -19,21 +20,34 @@ export default class SignInPage extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const { handleAuth } = this.props;
     const { username } = this.state;
     getSingleUser(username).then(({ user }) => {
-      navigate("/");
+      if (user) {
+        handleAuth(user);
+        navigate("/");
+      } else {
+        this.setState({ badInput: true });
+      }
     });
   };
 
   render() {
-    return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <label htmlFor="username">User: </label>
-        <input required id="username" onChange={this.handleChange} />
-        <label htmlFor="password">Password</label>
-        <input required type="password" id="password" onChange={this.handleChange} />
-        <button type="submit">Sign in</button>
-      </form>
+    const { user } = this.props;
+    const { badInput } = this.state;
+    return user ? (
+      <p>User profile...</p>
+    ) : (
+      <Fragment>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label htmlFor="username">User: </label>
+          <input required id="username" onChange={this.handleChange} />
+          <label htmlFor="password">Password</label>
+          <input required type="password" id="password" onChange={this.handleChange} />
+          <button type="submit">Sign in</button>
+        </form>
+        {badInput && <p>User not found</p>}
+      </Fragment>
     );
   }
 }
