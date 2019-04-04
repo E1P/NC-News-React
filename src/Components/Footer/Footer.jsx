f6e632b7efd1a138faf5816e5e3b614288326705
E1P <ewan.pearce@gmail.com>
2019-04-04 23:25:44 +0100

    Split text and space-apart

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="overlay-fader" />
-      <p className="footer-text">E1P NC News</p>
+      <p className="footer-text">E1P 2019</p>
+      <p className="footer-text">NC News</p>
     </div>
   );
 }
