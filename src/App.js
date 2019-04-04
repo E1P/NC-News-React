7f4f126557fadca2f6ce68687ebffef0a1d5734d
E1P <ewan.pearce@gmail.com>
2019-04-04 11:33:12 +0100

    Add user to props for all routes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import "./App.css";
 import { Footer, Header, NavBar, SignIn, MainPage, SignInPage, SingleArticle, Form } from "./Components/index";
 
 class App extends Component {
+  state = {
+    user: null
+  };
+
   render() {
+    const { user } = this.state;
     return (
       <div className="App fade-in">
         <Header />
-        <SignIn />
+        <SignIn user={user} />
         <NavBar />
         <Router className="router-main">
-          <MainPage path="/" />
-          <MainPage path="/articles" />
-          <MainPage path="/topics/:topic" />
-          <SingleArticle path="/articles/:article_id" />
-          <Form path="/form/:type/" />
-          <Form path="/form/:type/:article_id" />
-          <SignInPage path="/sign-in" />
+          <MainPage path="/" user={user} />
+          <MainPage path="/articles" user={user} />
+          <MainPage path="/topics/:topic" user={user} />
+          <SingleArticle path="/articles/:article_id" user={user} />
+          <Form path="/form/:type/" user={user} />
+          <Form path="/form/:type/:article_id" user={user} />
+          <SignInPage path="/sign-in" userAuth={this.userAuth} user={user} />
         </Router>
         <Footer />
       </div>
