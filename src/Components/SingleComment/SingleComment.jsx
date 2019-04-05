e9c651cb0b9eed6c247c25b728a2c50ee48cc7ea
E1P <ewan.pearce@gmail.com>
2019-04-05 15:30:11 +0100

    Import Voter and render with votes and props.handleVote

--- src/Components/SingleComment/SingleComment.jsx
+++ src/Components/SingleComment/SingleComment.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { DeleteButton } from "../index";
+import { DeleteButton, Voter } from "../index";
 
 export default function SingleComment(props) {
-  const { comment_id, author, body } = props.comment;
-  const { handleDelete } = props;
+  const { comment_id, author, body, votes } = props.comment;
+  const { handleDelete, handleVote } = props;
   return (
     <div className="comment">
       {" "}
       <h5>Author: {author}</h5>
       <p>{body}</p>
+      <Voter votes={votes} handleVote={handleVote} />
       <DeleteButton id={comment_id} handleDelete={handleDelete} />
     </div>
   );
