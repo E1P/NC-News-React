2e16e53713d0d3d0f0b076ac192ff9a5be37b34b
E1P <ewan.pearce@gmail.com>
2019-04-05 15:04:16 +0100

    Change div to button element

--- src/Components/DeleteButton/DeleteButton.jsx
+++ src/Components/DeleteButton/DeleteButton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function DeleteButton(props) {
   const { id, handleDelete } = props;
   return (
-    <div className="button" onClick={() => handleDelete(id)}>
+    <button className="button" onClick={() => handleDelete(id)}>
       Delete
-    </div>
+    </button>
   );
 }
