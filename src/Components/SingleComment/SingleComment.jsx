e503f7c8d0a7c200154086458e322e335003fcaa
E1P <ewan.pearce@gmail.com>
2019-04-05 15:05:32 +0100

    Remove unnecessary key prop and destructure handleDelete from props

--- src/Components/SingleComment/SingleComment.jsx
+++ src/Components/SingleComment/SingleComment.jsx
@@ -3,9 +3,9 @@ import { DeleteButton } from "../index";
 
 export default function SingleComment(props) {
   const { comment_id, author, body } = props.comment;
-  const handleDelete = props;
+  const { handleDelete } = props;
   return (
-    <div key={comment_id} className="comment">
+    <div className="comment">
       {" "}
       <h5>Author: {author}</h5>
       <p>{body}</p>
