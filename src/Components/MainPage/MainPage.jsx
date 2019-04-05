e56d3c5d8141f7cae21ae7be3ec0f12f97716a56
E1P <ewan.pearce@gmail.com>
2019-04-05 15:04:42 +0100

    Remove FormButton

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { MainArticles, TopArticles /* FormButton */ } from "../index";
+import { MainArticles, TopArticles } from "../index";
 
 export default function MainPage(props) {
   const { topic } = props;
   const limit = topic ? 1 : 3;
   return (
     <div>
-      {/* <div className="toolbar">
-        <FormButton type="article" />
-      </div> */}
       <TopArticles topic={topic} limit={limit} />
       <MainArticles topic={topic} />
     </div>
