9b57195f09e69a24426e0f69f994649f97a07d0b
E1P <ewan.pearce@gmail.com>
2019-04-07 00:22:56 +0100

    Changed votes, comments & created_at to p tags

--- src/Components/ArticlePreview/ArticlePreview.jsx
+++ src/Components/ArticlePreview/ArticlePreview.jsx
@@ -5,9 +5,9 @@ export default function ArticlePreview({ article }) {
     <section className="article-preview">
       <div>
         <h3>{article.title}</h3>
-        <h5>Votes:{article.votes}</h5>
-        <h5>Comments: {article.comment_count}</h5>
-        <h5>{article.created_at.slice(0, 10)}</h5>
+        <p>Votes:{article.votes}</p>
+        <p>Comments: {article.comment_count}</p>
+        <p>{article.created_at.slice(0, 10)}</p>
       </div>
     </section>
   );
