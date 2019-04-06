f2bdb8c5e751c24ed5f00b0239b0c184c38e3c91
E1P <ewan.pearce@gmail.com>
2019-04-06 14:30:16 +0100

    Write test and function for capitalising first letter of string

--- src/__tests__/aux-funcs.test.js
+++ src/__tests__/aux-funcs.test.js
@@ -1,10 +1,16 @@
-const { makeLowerCase } = require("../aux-funcs");
+const { makeLowerCase, capitaliseFirstLetter } = require("../aux-funcs");
 
 describe("Auxiliary functions", () => {
   describe("makeLowercase()", () => {
     it("returns string with all letters converted to lower case", () => {
       expect(makeLowerCase("Topic")).toBe("topic");
       expect(makeLowerCase("This Topic Has Spaces")).toBe("this topic has spaces");
+    });
+  });
+  describe("capitaliseFirstLetter()", () => {
+    it("capitalises first letter of given string", () => {
+      expect(capitaliseFirstLetter("topic")).toBe("Topic");
+      expect(capitaliseFirstLetter("topic-topic")).toBe("Topic-topic");
     });
   });
 });
--- src/aux-funcs.js
+++ src/aux-funcs.js
@@ -1,3 +1,7 @@
 exports.makeLowerCase = string => {
   return string.toLowerCase();
 };
+
+exports.capitaliseFirstLetter = string => {
+  return string[0].toUpperCase() + string.slice(1);
+};
