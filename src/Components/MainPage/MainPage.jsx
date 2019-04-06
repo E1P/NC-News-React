77fd79e56634fe7b87dae07b5a31429ea3a6aa88
E1P <ewan.pearce@gmail.com>
2019-04-06 15:09:37 +0100

    Add main-page class to container div

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -5,7 +5,7 @@ export default function MainPage(props) {
   const { topic } = props;
   const limit = topic ? 1 : 3;
   return (
-    <div>
+    <div className="main-page">
       <TopArticles topic={topic} limit={limit} />
       <MainArticles topic={topic} />
     </div>
