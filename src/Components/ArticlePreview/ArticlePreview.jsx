7c60596d945c62a0466ecccb0aad1f2e7506fc0f
E1P <ewan.pearce@gmail.com>
2019-04-07 01:19:49 +0100

    Remove unnecessary div

--- src/Components/ArticlePreview/ArticlePreview.jsx
+++ src/Components/ArticlePreview/ArticlePreview.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function ArticlePreview({ article }) {
   return (
     <section className="article-preview">
-      <div>
-        <h3>{article.title}</h3>
-        <p>Votes:{article.votes}</p>
-        <p>Comments: {article.comment_count}</p>
-        <p>{article.created_at.slice(0, 10)}</p>
-      </div>
+      {/* <div> */}
+      <h3>{article.title}</h3>
+      <p>Votes:{article.votes}</p>
+      <p>Comments: {article.comment_count}</p>
+      <p>{article.created_at.slice(0, 10)}</p>
+      {/* </div> */}
     </section>
   );
 }
