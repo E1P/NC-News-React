82161fc02cfe65dc356217f76e48db28f0bdaceb
E1P <ewan.pearce@gmail.com>
2019-04-08 15:12:52 +0100

    Add conditional for showing post article button

--- src/Components/FormButton/FormButton.jsx
+++ src/Components/FormButton/FormButton.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Link } from "@reach/router";
 
 export default function FormButton(props) {
-  const { type, article_id } = props;
-
-  return (
+  const { type, article_id, user, sender } = props;
+  return user ? (
     <div className="button">
       <Link to={`/form/${type}/${article_id || ""}`}>
         <div>Post {`${type}`}</div>
       </Link>
     </div>
+  ) : (
+    <Link to="/sign-in" sender={sender}>
+      <p>Sign in to post article</p>
+    </Link>
   );
 }
