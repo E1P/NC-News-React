ceecc84ce7e6830a7dfdcc155fae1e9be7df20ba
E1P <ewan.pearce@gmail.com>
2019-04-08 11:49:08 +0100

    Add user conditional to DeleteButton render. Send user to Voter props

--- src/Components/SingleComment/SingleComment.jsx
+++ src/Components/SingleComment/SingleComment.jsx
@@ -3,14 +3,14 @@ import { DeleteButton, Voter } from "../index";
 
 export default function SingleComment(props) {
   const { comment_id, author, body, votes } = props.comment;
-  const { handleDelete, handleVote } = props;
+  const { handleDelete, handleVote, user } = props;
   return (
     <div className="comment">
       {" "}
       <h5>Author: {author}</h5>
       <p>{body}</p>
-      <Voter votes={votes} handleVote={handleVote} />
-      <DeleteButton id={comment_id} handleDelete={handleDelete} />
+      <Voter votes={votes} handleVote={handleVote} user={user} />
+      {user === author && <DeleteButton id={comment_id} handleDelete={handleDelete} />}
     </div>
   );
 }
