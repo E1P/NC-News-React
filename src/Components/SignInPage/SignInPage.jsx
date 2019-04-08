9350fd109e271105cc500bc38a391986965f8b88
E1P <ewan.pearce@gmail.com>
2019-04-08 17:41:39 +0100

    Change classNames to better organise form inputs

--- src/Components/SignInPage/SignInPage.jsx
+++ src/Components/SignInPage/SignInPage.jsx
@@ -31,22 +31,29 @@ export default class SignInPage extends Component {
   };
 
   render() {
-    const { user } = this.props;
+    const { user, handleAuth } = this.props;
     const { disabled } = this.state;
-    return user ? (
-      <p>User profile...</p>
-    ) : (
-      <Fragment>
-        <form className="form" onSubmit={this.handleSubmit}>
-          <label htmlFor="username">User: </label>
-          <input required id="username" onChange={this.handleChange} />
-          <label htmlFor="password">Password</label>
-          <input required type="password" id="password" onChange={this.handleChange} />
-          <button disabled={disabled} className="button" type="submit">
-            Sign in
-          </button>
-        </form>
-      </Fragment>
+    return (
+      <div className="form">
+        {user ? (
+          <Fragment>
+            <p>User profile...</p>
+            <button className="button" onClick={() => handleAuth("")}>
+              Sign out
+            </button>
+          </Fragment>
+        ) : (
+          <form className="form-inputs" onSubmit={this.handleSubmit}>
+            <label htmlFor="username">User: </label>
+            <input required id="username" onChange={this.handleChange} />
+            <label htmlFor="password">Password</label>
+            <input required type="password" id="password" onChange={this.handleChange} />
+            <button disabled={disabled} className="button" type="submit">
+              Sign in
+            </button>
+          </form>
+        )}
+      </div>
     );
   }
 }
