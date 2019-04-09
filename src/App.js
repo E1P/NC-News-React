f81d612cf4e7152085745d1ee01272e663b774b5
E1P <ewan.pearce@gmail.com>
2019-04-09 10:29:01 +0100

    Add username property to state. Send this string down on props instead of user object.

--- src/App.js
+++ src/App.js
@@ -5,35 +5,46 @@ import { Footer, Header, NavBar, SignIn, MainPage, SignInPage, SingleArticle, Fo
 
 class App extends Component {
   state = {
-    user: null
+    user: null,
+    username: ""
   };
 
   componentDidMount() {
-    console.log("Storage >>> ", localStorage);
-    const user = localStorage.getItem("user");
-    if (user) this.setState({ user });
+    console.log("Storage on mount>>> ", localStorage);
+    const username = localStorage.getItem("nc_news_user");
+    if (username && username !== "undefined") this.setState({ username });
   }
 
+  // componentDidUpdate(_, prevState) {
+  //   console.log("Storage on update >>> ", localStorage);
+  //   const user = localStorage.getItem("user");
+
+  //   if (user.username !== prevState.user.username) this.setState({ user });
+  // }
+
   handleAuth = user => {
-    this.setState({ user });
-    localStorage.setItem("user", user);
+    console.log("handleAuth in App: ", user);
+    this.setState({ user, username: user.username });
+    localStorage.setItem("nc_news_user", user.username);
   };
 
   render() {
-    const { user } = this.state;
+    const { user, username } = this.state;
     const { handleAuth } = this;
+    console.log("user in app: ", user);
+    console.log("username in app: ", username);
     return (
       <div className="App fade-in">
         <Header />
-        <SignIn user={user} handleAuth={handleAuth} />
+        <SignIn username={username} handleAuth={handleAuth} />
         <NavBar />
         <Router className="router">
-          <MainPage path="/" user={user} />
-          <MainPage path="/articles" user={user} />
-          <MainPage path="/topics/:topic" user={user} />
-          <SingleArticle path="/articles/:article_id" user={user} />
-          <Form path="/form/:type/" user={user} />
-          <Form path="/form/:type/:article_id" user={user} />
+          <MainPage path="/" username={username} />
+          <MainPage path="/articles" username={username} />
+          <MainPage path="/topics/:topic" username={username} />
+          <SingleArticle path="/articles/:article_id" username={username} />
+          <Form path="/form/:type/" username={username} />
+          <Form path="/form/:type/:article_id" username={username} />
           <SignInPage path="/sign-in" handleAuth={handleAuth} user={user} />
         </Router>
         <Footer />
