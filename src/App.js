6c2556bb594656af66d24b3ff254dd306274ce3f
E1P <ewan.pearce@gmail.com>
2019-04-30 14:05:52 +0100

    Add allLoaded method. Adjust scrolling to accomodate.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,20 @@ class App extends Component {
     username: "",
     currentTopic: "all",
     p: 1,
-    hidden: true
+    hidden: true,
+    justLoaded: false,
+    allLoaded: false
   };
 
   componentDidMount() {
     const username = localStorage.getItem("nc_news_user");
     if (username && username !== "undefined") this.setState({ username, p: 1 });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.justLoaded) {
+      this.setState({ justLoaded: false });
+    }
   }
 
   handleAuth = user => {
@@ -26,13 +34,17 @@ class App extends Component {
     this.setState({ currentTopic: topic, p: 1 });
   };
 
+  handleAllLoaded = input => {
+    this.setState({ allLoaded: input });
+  };
+
   handleScroll = event => {
     event.persist();
-    const { p } = this.state;
+    const { p, justLoaded, allLoaded } = this.state;
     const { clientHeight, scrollHeight, scrollTop } = event.target;
-    if (clientHeight + scrollTop === scrollHeight) {
+    if (clientHeight + scrollTop === scrollHeight && !justLoaded && !allLoaded) {
       const pageToLoad = p + 1;
-      this.setState({ p: pageToLoad, isLoaded: false });
+      this.setState({ p: pageToLoad, justLoaded: true });
     }
   };
 
@@ -49,16 +61,16 @@ class App extends Component {
 
   render() {
     const { user, username, currentTopic, p, hidden } = this.state;
-    const { handleAuth, handleTopicChange, handleDropdownClick, handleScroll, handleOutsideDropDownClick } = this;
+    const { handleAuth, handleTopicChange, handleDropdownClick, handleScroll, handleOutsideDropDownClick, handleAllLoaded } = this;
     return (
       <div className="App fade-in" onScroll={handleScroll} onClick={handleOutsideDropDownClick}>
         <Header />
         <SignIn username={username} handleAuth={handleAuth} handleDropdownClick={handleDropdownClick} hidden={hidden} />
         <NavBar topic={currentTopic} handleTopicChange={handleTopicChange} />
         <Router className="router">
-          <MainPage path="/" p={p} username={username} handleTopicChange={handleTopicChange} />
-          <MainPage path="/all" p={p} username={username} handleTopicChange={handleTopicChange} />
-          <MainPage path="/topics/:topic" p={p} username={username} handleTopicChange={handleTopicChange} />
+          <MainPage path="/" p={p} username={username} handleTopicChange={handleTopicChange} handleAllLoaded={handleAllLoaded} />
+          <MainPage path="/all" p={p} username={username} handleTopicChange={handleTopicChange} handleAllLoaded={handleAllLoaded} />
+          <MainPage path="/topics/:topic" p={p} username={username} handleTopicChange={handleTopicChange} handleAllLoaded={handleAllLoaded} />
           <SingleArticle path="/articles/:article_id" username={username} handleTopicChange={handleTopicChange} />
           <Form path="/form/:type/" username={username} />
           <Form path="/form/:type/:article_id" username={username} />
