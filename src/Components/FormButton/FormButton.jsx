2e0c0992f593e480e5e3d8de79ab6bc06745deb7
E1P <ewan.pearce@gmail.com>
2019-05-09 14:14:20 +0100

    Add button class

--- src/Components/FormButton/FormButton.jsx
+++ src/Components/FormButton/FormButton.jsx
@@ -5,11 +5,11 @@ export default function FormButton(props) {
   const { type, article_id, username } = props;
   return username ? (
     <Link to={`/form/${type}/${article_id || ""}`}>
-      <button>Post {`${type}`}</button>
+      <button className="button">Post {`${type}`}</button>
     </Link>
   ) : (
     <Link to="/sign-in">
-      <button>Sign in to post {type}</button>
+      <button className="button">Sign in to post {type}</button>
     </Link>
   );
 }
